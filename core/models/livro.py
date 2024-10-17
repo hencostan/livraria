c4f29b0b92036e779806df847a0deb112825d2d5
Henrique Costa <hencostnascimento@gmail.com>
2024-10-17 10:15:43 -0300

    Adiciona relacionamento de Livro com Categoria e Editora

--- core/models/livro.py
+++ core/models/livro.py
@@ -5,6 +5,18 @@ class Livro(models.Model):
     isbn = models.CharField(max_length=32, null=True, blank=True)
     quantidade = models.IntegerField(default=0,  null=True, blank=True)
     preco = models.DecimalField(max_digits=7, decimal_places=2, default=0, null=True, blank=True)
+    
+    from .categoria import Categoria
+
+    categoria = models.ForeignKey(
+        Categoria, on_delete=models.PROTECT, related_name="livros", null=True, blank=True
+    )
+
+    from .editora import Editora
+
+    editora = models.ForeignKey(
+        Editora, on_delete=models.PROTECT, related_name="livros", null=True, blank=True
+    )
 
     def __str__(self):
         return f"({self.id}) {self.titulo} ({self.quantidade})"
