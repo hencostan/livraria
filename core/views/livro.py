6fbdd9526f278c489e31e38c859825f5d0bf5756
Henrique Costa <hencostnascimento@gmail.com>
2024-10-17 10:40:46 -0300

    Múltiplos serializadores para Livro

--- core/views/livro.py
+++ core/views/livro.py
@@ -1,13 +1,15 @@
 from rest_framework.viewsets import ModelViewSet
 
 from core.models import Categoria, Editora, Autor, Livro
-from core.serializers import CategoriaSerializer, EditoraSerializer, AutorSerializer, LivroSerializer, LivroDetailSerializer
+from core.serializers import CategoriaSerializer, EditoraSerializer, AutorSerializer, LivroSerializer, LivroDetailSerializer, LivroListSerializer
 
 class LivroViewSet(ModelViewSet):
     queryset = Livro.objects.all()
     serializer_class = LivroSerializer
 
     def get_serializer_class(self):
-        if self.action in ["list", "retrieve"]:
+        if self.action == "list":
+            return LivroListSerializer
+        elif self.action == "retrieve":
             return LivroDetailSerializer
         return LivroSerializer
