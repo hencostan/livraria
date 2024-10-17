28676bab3f2ebf7003d9052d0358d930cd13e6a1
Henrique Costa <hencostnascimento@gmail.com>
2024-10-17 08:56:05 -0300

    Criação da API para Editora

--- core/serializers/categoria.py
+++ core/serializers/categoria.py
@@ -3,7 +3,7 @@ from rest_framework.serializers import ModelSerializer
 from core.models import Categoria
 
 
-class CategoriaSerializer(ModelSerializer)
+class CategoriaSerializer(ModelSerializer):
     class Meta:
-            model = Categoria
+        model = Categoria
         fields = "__all__"
--- core/views/__init__.py
+++ core/views/__init__.py
@@ -1,2 +1,4 @@
 from .user import UserViewSet
 from .categoria import CategoriaViewSet
+from .editora import EditoraViewSet
+
