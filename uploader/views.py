12dbbd752fb3f826e7f0d5532d603e4dae307d02
Henrique Costa <hencostnascimento@gmail.com>
2024-11-25 09:35:13 -0300

    Inclusão da foto de perfil no usuário

--- uploader/views.py
+++ uploader/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import mixins, parsers, viewsets
 
-# Create your views here.
+from uploader.models import Document, Image
+from uploader.serializers import DocumentUploadSerializer, ImageUploadSerializer
+
+
+class CreateViewSet(mixins.ListModelMixin, mixins.CreateModelMixin, viewsets.GenericViewSet):
+    pass
+
+
+class DocumentUploadViewSet(CreateViewSet):
+    queryset = Document.objects.all() #  pylint: disable=no-member
+    serializer_class = DocumentUploadSerializer
+    parser_classes = [parsers.FormParser, parsers.MultiPartParser]
+
+
+class ImageUploadViewSet(CreateViewSet):
+    queryset = Image.objects.all() #  pylint: disable=no-member
+    serializer_class = ImageUploadSerializer
+    parser_classes = [parsers.FormParser, parsers.MultiPartParser]
