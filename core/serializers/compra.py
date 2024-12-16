f774edb6389206631e715cee0b147d328632ab9f
Henrique Costa <hencostnascimento@gmail.com>
2024-12-16 08:52:22 -0300

    Gravando o preço do livro no item da compra

--- core/serializers/compra.py
+++ core/serializers/compra.py
@@ -23,6 +23,16 @@ class CompraSerializer(ModelSerializer):
         itens = ItensCompraSerializer(many=True, read_only=True)
         fields = ("id", "usuario", "status", "total", "itens")
 
+    def update(self, compra, validated_data):
+        itens = validated_data.pop("itens")
+        if itens:
+            compra.itens.all().delete()
+            for item in itens:
+                item["preco"] = item["livro"].preco  # nova linha
+                ItensCompra.objects.create(compra=compra, **item)
+        compra.save()
+        return super().update(compra, validated_data)
+
 class ItensCompraCreateUpdateSerializer(ModelSerializer):
     class Meta:
         model = ItensCompra
@@ -48,6 +58,15 @@ class CompraCreateUpdateSerializer(ModelSerializer):
             for item_data in itens_data:
                 ItensCompra.objects.create(compra=compra, **item_data)
         return super().update(compra, validated_data)
+
+    def create(self, validated_data):
+        itens = validated_data.pop("itens")
+        compra = Compra.objects.create(**validated_data)
+        for item in itens:
+            item["preco"] = item["livro"].preco # nova linha
+            ItensCompra.objects.create(compra=compra, **item)
+        compra.save()
+        return compra
     
 class ItensCompraListSerializer(ModelSerializer):
     livro = CharField(source="livro.titulo", read_only=True)
